fb4338684d95adf0f4fce2066ee58028407a564a
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2023-02-11 22:40:12 -0800

    CharacterMovement

---
+++ Environment/Assets/Scripts/CharacterMoveScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveScript : MonoBehaviour
+{
+    public Vector2 input;
+    public CharacterController controller;
+    public Vector3 direction;
+    public float speed;
+    private float turnsmoothtime = 0.05f;
+    private float turnsmoothvelocity = 1;
+    public float gravity = -9.81f;
+    public float gravitymulti = 3f;
+    private float velocity;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(input.x, 0.0f, input.y);
+         animator.SetFloat("speed", input.sqrMagnitude);
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    private void ApplyGravity()
+    {
+        if(controller.isGrounded && velocity < 0f)
+        {
+            velocity = -1f;
+        }
+        else
+        {
+            velocity += gravity * gravitymulti * Time.deltaTime;
+        }
+
+        direction.y = velocity;
+    }
+
+    private void ApplyRotation()
+    {
+        if(input.sqrMagnitude == 0) return;
+
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnsmoothvelocity, turnsmoothtime);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+    }
+
+    private void ApplyMovement()
+    {
+        controller.Move(direction * speed * Time.deltaTime);
+    }
+}

cc377eb6a5b47f8afc1760a2acff96004b85432a
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2023-02-17 15:40:21 -0800

    Predefined Locations + AutoWalk + InteractHarvest Animation

--- Environment/Assets/Scripts/CharacterMoveScript.cs
+++ Environment/Assets/Scripts/CharacterMoveScript.cs
@@ -4,24 +4,87 @@ using UnityEngine;
 
 public class CharacterMoveScript : MonoBehaviour
 {
-    public Vector2 input;
+    public Transform parentinteractionpoint;
+    private Transform interactionpoint;
+    private bool inrange;
+    public Animator animator;
     public CharacterController controller;
-    public Vector3 direction;
+    private Vector2 input;
+    private Vector3 direction;
     public float speed;
     private float turnsmoothtime = 0.05f;
-    private float turnsmoothvelocity = 1;
-    public float gravity = -9.81f;
-    public float gravitymulti = 3f;
+    private float turnsmoothvelocity = 1f;
+    private float gravity = -9.81f;
+    private float gravitymulti = 3f;
     private float velocity;
-    public Animator animator;
+
+    void Start()
+    {
+        // initially, stay at current place(which is itself)
+        interactionpoint = controller.transform;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Interactable"))
+        {
+            inrange = true;
+            Debug.Log("player in range");
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Interactable"))
+        {
+            inrange = false;
+            Debug.Log("player not in range");
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
-        direction = new Vector3(input.x, 0.0f, input.y);
-         animator.SetFloat("speed", input.sqrMagnitude);
+        // input.x = Input.GetAxisRaw("Horizontal");
+        // input.y = Input.GetAxisRaw("Vertical");
+        if(inrange && Input.inputString == "e")
+        {
+            animator.SetBool("harvesting", true);
+        }
+
+        if(Input.anyKeyDown)
+        {
+            if(Input.inputString == "1")
+            {
+                interactionpoint = parentinteractionpoint.GetChild(0);
+            }
+            if(Input.inputString == "2")
+            {
+                interactionpoint = parentinteractionpoint.GetChild(1);
+            }
+            if(Input.inputString == "3")
+            {
+                interactionpoint = parentinteractionpoint.GetChild(2);
+            }
+            if(Input.inputString == "4")
+            {
+                interactionpoint = parentinteractionpoint.GetChild(3);
+            }
+        }
+
+        if(Vector3.Distance(interactionpoint.transform.position, controller.transform.position) > 0.9)
+        {
+            animator.SetBool("harvesting", false);
+            input = new Vector2(interactionpoint.transform.position.x - controller.transform.position.x, interactionpoint.transform.position.z - controller.transform.position.z);
+        }
+        else
+        {
+            input = new Vector2(0, 0);
+        }
+        input.Normalize();
+        animator.SetFloat("speed", input.sqrMagnitude);
+        direction = new Vector3(input.x, 0, input.y);
+        
         ApplyGravity();
         ApplyRotation();
         ApplyMovement();
@@ -29,9 +92,9 @@ public class CharacterMoveScript : MonoBehaviour
 
     private void ApplyGravity()
     {
-        if(controller.isGrounded && velocity < 0f)
+        if(controller.isGrounded && velocity < 0)
         {
-            velocity = -1f;
+            velocity = -1;
         }
         else
         {

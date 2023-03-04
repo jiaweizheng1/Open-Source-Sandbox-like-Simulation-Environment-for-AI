f99ada55fe81d87554a23c32a48cedc257531a43
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2023-03-04 15:35:13 -0800

    Logic For Campfire, Workbench, Rocket Interaction

--- Environment/Assets/Scripts/CharacterMoveScript.cs
+++ Environment/Assets/Scripts/CharacterMoveScript.cs
@@ -7,18 +7,18 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using System;
+using Random=UnityEngine.Random;
 
 public class CharacterMoveScript : Agent
 {
     public float speed;
-    public Transform trees, farm, pool, rocks;
     public Animator animator;
     public CharacterController controller;
-    public TMP_Text log_t, food_t, droplet_t, iron_t;
-
-    private int log, food, droplet, iron;
-    private Transform target;
+    public Transform treeslocation, farmlocation, poollocation, rockslocation, benchlocation, firelocation, rocketlocation;
+    public TMP_Text log_t, apple_t, meat_t, oil_t, water_t, copper_t, gold_t, iron_t;
+    private int log, apple, meat, oil, water, copper, gold, iron;
     private bool moving, busy;
+    private Transform target;
     private Vector2 input;
     private Vector3 direction;
     private float turnsmoothtime = 0.05f, turnsmoothvelocity = 1f;
@@ -44,6 +44,15 @@ public class CharacterMoveScript : Agent
     public float timemulti;
     public TMP_Text time_t;
     public TMP_Text day_t;
+
+    private bool benchbuilt, campfirebuilt, rocketbuilt;
+    public GameObject bench, fire, rocket;
+    public GameObject benchui, fireui, rocketui;
+    private int[] benchbuildmats = {3, 1, 1, 1};
+    private int[] firebuildmats = {2, 1};
+    private int[] cookmats = {1, 1, 1, 1};
+    private int[] rocketbuildmats = {10, 10, 7, 10};
+    private int[] rocketlaunchmats = {1, 5, 5, 10, 1, 1, 1};
     
     public override void OnEpisodeBegin()
     {
@@ -60,12 +69,35 @@ public class CharacterMoveScript : Agent
         animator.SetBool("harvesting", false);
         animator.SetFloat("speed", 0);
 
+        benchbuilt = false;
+        bench.transform.Find("BenchBlueprint").gameObject.SetActive(true);
+        bench.transform.Find("Bench").gameObject.SetActive(false);
+        benchui.transform.Find("UIBuild").gameObject.SetActive(true);
+        benchui.transform.Find("UIBuilt").gameObject.SetActive(false);
+        animator.SetBool("harvesting", false);
+
+        campfirebuilt = false;
+        fire.transform.Find("FireBlueprint").gameObject.SetActive(true);
+        fire.transform.Find("Fire").gameObject.SetActive(false);
+        fireui.transform.Find("UIBuild").gameObject.SetActive(true);
+        fireui.transform.Find("UICook").gameObject.SetActive(false);
+
+        rocketbuilt = false;
+
         log = 0;
         log_t.text = "x" + log;
-        food = 0;
-        food_t.text = "x" + food;
-        droplet = 0;
-        droplet_t.text = "x" + droplet;
+        apple = 0;
+        apple_t.text = "x" + apple;
+        meat = 0;
+        meat_t.text = "x" + meat;
+        oil = 0;
+        oil_t.text = "x" + oil;
+        water = 0;
+        water_t.text = "x" + water;
+        copper = 0;
+        copper_t.text = "x" + copper;
+        gold = 0;
+        gold_t.text = "x" + gold;
         iron = 0;
         iron_t.text = "x" + iron;
 
@@ -77,13 +109,19 @@ public class CharacterMoveScript : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(controller.transform.position);
-        sensor.AddObservation(trees);
-        sensor.AddObservation(farm);
-        sensor.AddObservation(pool);
-        sensor.AddObservation(rocks);
+
+        sensor.AddObservation(treeslocation);
+        sensor.AddObservation(farmlocation);
+        sensor.AddObservation(poollocation);
+        sensor.AddObservation(rockslocation);
+
         sensor.AddObservation(log);
-        sensor.AddObservation(food);
-        sensor.AddObservation(droplet);
+        sensor.AddObservation(apple);
+        sensor.AddObservation(meat);
+        sensor.AddObservation(oil);
+        sensor.AddObservation(water);
+        sensor.AddObservation(copper);
+        sensor.AddObservation(gold);
         sensor.AddObservation(iron);
         //Add all the important observations like HP, food, thirst, inventory items
     }
@@ -104,55 +142,34 @@ public class CharacterMoveScript : Agent
         busy = true;
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
-        food++;
-        food_t.text = "x" + food;
+        int whichfood = Random.Range(0, 2);
+        if(whichfood == 0)
+        {
+            apple++;
+            apple_t.text = "x" + apple;
+        }
+        else
+        {
+            meat++;
+            meat_t.text = "x" + meat;
+        }
+        int chanceoil = Random.Range(0, 10);
+        if(chanceoil <= 2)
+        {
+            oil++;
+            oil_t.text = "x" + oil;
+        }
         animator.SetBool("harvesting", false);
         busy = false;
     }
 
-    IEnumerator Consumefood()
+    IEnumerator CollectWater()
     {
         busy = true;
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
-        food--;
-        if(Hunger + 20 > MaxHunger){
-            Hunger=MaxHunger;
-        }
-        else{
-            Hunger += 20;
-        }
-
-        food_t.text = "x" + food;
-        animator.SetBool("harvesting", false);
-        busy = false;
-    }
-
-
-    IEnumerator Collectdroplets()
-    {
-        busy = true;
-        animator.SetBool("harvesting", true);
-        yield return new WaitForSeconds(2);
-        droplet++;
-        
-        droplet_t.text = "x" + droplet;
-        animator.SetBool("harvesting", false);
-        busy = false;
-    }
-    IEnumerator Consumedroplets()
-    {
-        busy = true;
-        animator.SetBool("harvesting", true);
-        yield return new WaitForSeconds(2);
-        droplet--;
-        if(Thirst + 20 > MaxThirst){
-            Thirst=MaxThirst;
-        }
-        else{
-            Thirst += 20;
-        }
-        droplet_t.text = "x" + droplet;
+        water++;
+        water_t.text = "x" + water;
         animator.SetBool("harvesting", false);
         busy = false;
     }
@@ -162,8 +179,76 @@ public class CharacterMoveScript : Agent
         busy = true;
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
-        iron++;
-        iron_t.text = "x" + iron;
+        int whichmineral = Random.Range(0, 3);
+        if(whichmineral == 0)
+        {
+            copper++;
+            copper_t.text = "x" + copper;
+        }
+        else if(whichmineral == 1)
+        {
+            gold++;
+            gold_t.text = "x" + gold;
+        }
+        else
+        {
+            iron++;
+            iron_t.text = "x" + iron;
+        }
+        animator.SetBool("harvesting", false);
+        busy = false;
+    }
+
+    IEnumerator BuildBench()
+    {
+        busy = true;
+        animator.SetBool("harvesting", true);
+        yield return new WaitForSeconds(2);
+        benchbuilt = true;
+        bench.transform.Find("BenchBlueprint").gameObject.SetActive(false);
+        bench.transform.Find("Bench").gameObject.SetActive(true);
+        benchui.transform.Find("UIBuild").gameObject.SetActive(false);
+        benchui.transform.Find("UIBuilt").gameObject.SetActive(true);
+        animator.SetBool("harvesting", false);
+        busy = false;
+    }
+
+    IEnumerator BuildFire()
+    {
+        busy = true;
+        animator.SetBool("harvesting", true);
+        yield return new WaitForSeconds(2);
+        campfirebuilt = true;
+        fire.transform.Find("FireBlueprint").gameObject.SetActive(false);
+        fire.transform.Find("Fire").gameObject.SetActive(true);
+        fireui.transform.Find("UIBuild").gameObject.SetActive(false);
+        fireui.transform.Find("UICook").gameObject.SetActive(true);
+        animator.SetBool("harvesting", false);
+        busy = false;
+    }
+
+    IEnumerator Cook()
+    {
+        busy = true;
+        animator.SetBool("harvesting", true);
+        yield return new WaitForSeconds(2);
+        Health = MaxHealth;
+        Hunger = MaxHunger;
+        Thirst = MaxThirst;
+        animator.SetBool("harvesting", false);
+        busy = false;
+    }
+
+    IEnumerator BuildRocket()
+    {
+        busy = true;
+        animator.SetBool("harvesting", true);
+        yield return new WaitForSeconds(2);
+        campfirebuilt = true;
+        rocket.transform.Find("RocketBlueprint").gameObject.SetActive(false);
+        rocket.transform.Find("Rocket").gameObject.SetActive(true);
+        rocketui.transform.Find("UIBuild").gameObject.SetActive(false);
+        rocketui.transform.Find("UILaunch").gameObject.SetActive(true);
         animator.SetBool("harvesting", false);
         busy = false;
     }
@@ -182,7 +267,7 @@ public class CharacterMoveScript : Agent
         {
             if(vetcaction.DiscreteActions[0] == 0)
             {
-                if(needtomove(trees))
+                if(needtomove(treeslocation))
                 {
                 }
                 else
@@ -192,7 +277,7 @@ public class CharacterMoveScript : Agent
             }
             if(vetcaction.DiscreteActions[0] == 1)
             {
-                if(needtomove(farm))
+                if(needtomove(farmlocation))
                 { 
                 }
                 else
@@ -202,17 +287,17 @@ public class CharacterMoveScript : Agent
             }
             if(vetcaction.DiscreteActions[0] == 2)
             {
-                if(needtomove(pool))
+                if(needtomove(poollocation))
                 { 
                 }
                 else
                 {
-                    StartCoroutine(Collectdroplets());
+                    StartCoroutine(CollectWater());
                 }
             }
             if(vetcaction.DiscreteActions[0] == 3)
             {
-                if(needtomove(rocks))
+                if(needtomove(rockslocation))
                 {
                 }
                 else
@@ -222,17 +307,56 @@ public class CharacterMoveScript : Agent
             }
             if(vetcaction.DiscreteActions[0] == 4)
             {
-                if(food>0){
-                    StartCoroutine(Consumefood());
+                if(needtomove(benchlocation))
+                {
                 }
-                
+                else if(!benchbuilt && log >= benchbuildmats[0] && copper >= benchbuildmats[1] && gold >= benchbuildmats[2] && iron >= benchbuildmats[3])
+                {
+                    StartCoroutine(BuildBench());
+                    log -= benchbuildmats[0];
+                    copper -= benchbuildmats[1];
+                    gold -= benchbuildmats[2];
+                    iron -= benchbuildmats[3];
+                }
             }
             if(vetcaction.DiscreteActions[0] == 5)
             {
-                if(droplet>0){
-                    StartCoroutine(Consumedroplets());
+                if(needtomove(firelocation))
+                {
                 }
-                
+                else if(!campfirebuilt && log >= firebuildmats[0] && iron >= firebuildmats[1])
+                {
+                    StartCoroutine(BuildFire());
+                    log -= firebuildmats[0];
+                    iron -= firebuildmats[1];
+                }
+                else if(campfirebuilt && log >= cookmats[0] && apple >= cookmats[1] && meat >= cookmats[2] && water >= cookmats[3])
+                {
+                    StartCoroutine(Cook());
+                    log -= cookmats[0];
+                    apple -= cookmats[1];
+                    meat -= cookmats[2];
+                    water -= cookmats[3];
+                }
+            }
+            if(vetcaction.DiscreteActions[0] == 6)
+            {
+                if(needtomove(rocketlocation))
+                {
+                }
+                else if(!rocketbuilt && log >= rocketbuildmats[0] && copper >= rocketbuildmats[1] && gold >= rocketbuildmats[2] && iron >= rocketbuildmats[3])
+                {
+                    StartCoroutine(BuildRocket());
+                    log -= rocketbuildmats[0];
+                    copper -= rocketbuildmats[1];
+                    gold -= rocketbuildmats[2];
+                    iron -= rocketbuildmats[3];
+
+                }
+                else if(rocketbuilt && log >= rocketbuildmats[0] && apple >= rocketbuildmats[1] && meat >= rocketbuildmats[2] && oil >= rocketbuildmats[3] 
+                && copper >= rocketbuildmats[4] && gold >= rocketbuildmats[5] && iron >= rocketbuildmats[6])
+                {
+                }
             }
         }
     }
@@ -263,6 +387,10 @@ public class CharacterMoveScript : Agent
         else if(Input.GetKey(KeyCode.Y))
         {
             discreteactions[0] = 5;
+        }
+        else if(Input.GetKey(KeyCode.U))
+        {
+            discreteactions[0] = 6;
         }
         else
         {
@@ -322,10 +450,10 @@ public class CharacterMoveScript : Agent
             Thirst = Thirst - BarSpeedMulti * Time.deltaTime;
         }
         if(Health > 0 && HungerSlider.value == 0){
-            Health = Health - BarSpeedMulti * 2 * Time.deltaTime;
+            Health = Health - BarSpeedMulti * Time.deltaTime;
         }
         if(Health > 0 && ThirstSlider.value == 0){
-            Health = Health - BarSpeedMulti * 2 * Time.deltaTime;
+            Health = Health - BarSpeedMulti * Time.deltaTime;
         }
         if(Health < 0)
         {

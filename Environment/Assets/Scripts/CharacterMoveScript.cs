a1d496ca7eba3098d2cfafe3690339544219da52
Vincent-WangCH <wch152346@gmail.com>
2023-03-06 03:06:00 -0800

    crafting_90%

--- Environment/Assets/Scripts/CharacterMoveScript.cs
+++ Environment/Assets/Scripts/CharacterMoveScript.cs
@@ -23,7 +23,7 @@ public class CharacterMoveScript : Agent
     private Vector3 direction;
     private float turnsmoothtime = 0.1f, turnsmoothvelocity = 0.1f;
     private float gravity = -9.81f, gravitymulti = 3f, velocity;
-    
+
     public float BarSpeedMulti;
     [Header("Player Health")]
     private float MaxHealth = 100f;
@@ -48,12 +48,16 @@ public class CharacterMoveScript : Agent
     private bool benchbuilt, campfirebuilt, rocketbuilt;
     public GameObject bench, fire, rocket;
     public GameObject benchui, fireui, rocketui;
-    private int[] benchbuildmats = {3, 1, 1, 1};
-    private int[] firebuildmats = {2, 1};
-    private int[] cookmats = {1, 1, 1, 1};
-    private int[] rocketbuildmats = {10, 10, 7, 10};
-    private int[] rocketlaunchmats = {1, 5, 5, 10, 1, 1, 1};
-    
+    private int[] benchbuildmats = { 3, 1, 1, 1 };
+    private int[] firebuildmats = { 2, 1 };
+    private int[] cookmats = { 1, 1, 1, 1 };
+    private int[] rocketbuildmats = { 10, 10, 7, 10 };
+    private int[] rocketlaunchmats = { 1, 5, 5, 10, 1, 1, 1 };
+
+    public GameObject toolSelector;
+    private int[] toolChoose = { 350, 400, 450, 500, 550, 600 };
+    private int log_tool, apple_tool, meat_tool, oil_tool, water_tool, copper_tool, gold_tool, iron_tool;
+
     public override void OnEpisodeBegin()
     {
         //initially, stay at current place(which is itself)
@@ -105,6 +109,15 @@ public class CharacterMoveScript : Agent
         Health = MaxHealth;
         Hunger = MaxHunger;
         Thirst = MaxThirst;
+
+        log_tool = 0;
+        apple_tool = 0;
+        meat_tool = 0;
+        oil_tool = 0;
+        water_tool = 0;
+        copper_tool = 0;
+        gold_tool = 0;
+        iron_tool = 0;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -132,7 +145,8 @@ public class CharacterMoveScript : Agent
         busy = true;
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
-        log++;
+        Debug.Log(log_tool);
+        log += 1 + log_tool;
         ManualUpdateAllText();
         animator.SetBool("harvesting", false);
         busy = false;
@@ -144,18 +158,18 @@ public class CharacterMoveScript : Agent
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
         int whichfood = Random.Range(0, 2);
-        if(whichfood == 0)
+        if (whichfood == 0)
         {
-            apple++;
+            apple += 1 + apple_tool;
         }
         else
         {
-            meat++;
+            meat += 1 + meat_tool;
         }
         int chanceoil = Random.Range(0, 10);
-        if(chanceoil <= 2)
+        if (chanceoil <= 2)
         {
-            oil++;
+            oil += 1 + oil_tool;
         }
         ManualUpdateAllText();
         animator.SetBool("harvesting", false);
@@ -167,7 +181,7 @@ public class CharacterMoveScript : Agent
         busy = true;
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
-        water++;
+        water += 1 + water_tool;
         ManualUpdateAllText();
         animator.SetBool("harvesting", false);
         busy = false;
@@ -179,17 +193,17 @@ public class CharacterMoveScript : Agent
         animator.SetBool("harvesting", true);
         yield return new WaitForSeconds(2);
         int whichmineral = Random.Range(0, 3);
-        if(whichmineral == 0)
+        if (whichmineral == 0)
         {
-            copper++;
+            copper += 1 + copper_tool;
         }
-        else if(whichmineral == 1)
+        else if (whichmineral == 1)
         {
-            gold++;
+            gold += 1 + gold_tool;
         }
         else
         {
-            iron++;
+            iron += 1 + iron_tool;
         }
         ManualUpdateAllText();
         animator.SetBool("harvesting", false);
@@ -270,7 +284,7 @@ public class CharacterMoveScript : Agent
 
     IEnumerator Wave()
     {
-        direction = new Vector3(-1,0,-3);
+        direction = new Vector3(-1, 0, -3);
         animator.SetBool("waving", true);
         yield return new WaitForSeconds(2);
         animator.SetBool("waving", false);
@@ -290,7 +304,7 @@ public class CharacterMoveScript : Agent
     {
         rocket.transform.Find("BigExplosionEffect").gameObject.SetActive(true);
         rocket.transform.Find("SmokeEffect").gameObject.SetActive(true);
-        for(int i = 1; i <= 200; i++)
+        for (int i = 1; i <= 200; i++)
         {
             rocket.transform.Find("Rocket").Translate(0, -0.2f, 0);
             rocket.transform.Find("SmokeEffect").Translate(0, 0.2f, 0);
@@ -331,11 +345,11 @@ public class CharacterMoveScript : Agent
 
     public override void OnActionReceived(ActionBuffers vetcaction)
     {
-        if(!moving && !busy)
+        if (!moving && !busy)
         {
-            if(vetcaction.DiscreteActions[0] == 0)
+            if (vetcaction.DiscreteActions[0] == 0)
             {
-                if(needtomove(treeslocation))
+                if (needtomove(treeslocation))
                 {
                 }
                 else
@@ -343,29 +357,29 @@ public class CharacterMoveScript : Agent
                     StartCoroutine(Log());
                 }
             }
-            if(vetcaction.DiscreteActions[0] == 1)
+            if (vetcaction.DiscreteActions[0] == 1)
             {
-                if(needtomove(farmlocation))
-                { 
+                if (needtomove(farmlocation))
+                {
                 }
                 else
                 {
                     StartCoroutine(Gatherfood());
                 }
             }
-            if(vetcaction.DiscreteActions[0] == 2)
+            if (vetcaction.DiscreteActions[0] == 2)
             {
-                if(needtomove(poollocation))
-                { 
+                if (needtomove(poollocation))
+                {
                 }
                 else
                 {
                     StartCoroutine(CollectWater());
                 }
             }
-            if(vetcaction.DiscreteActions[0] == 3)
+            if (vetcaction.DiscreteActions[0] == 3)
             {
-                if(needtomove(rockslocation))
+                if (needtomove(rockslocation))
                 {
                 }
                 else
@@ -373,42 +387,42 @@ public class CharacterMoveScript : Agent
                     StartCoroutine(Mine());
                 }
             }
-            if(vetcaction.DiscreteActions[0] == 4)
+            if (vetcaction.DiscreteActions[0] == 4)
             {
-                if(needtomove(benchlocation))
+                if (needtomove(benchlocation))
                 {
                 }
-                else if(!benchbuilt && log >= benchbuildmats[0] && copper >= benchbuildmats[1] && gold >= benchbuildmats[2] && iron >= benchbuildmats[3])
+                else if (!benchbuilt && log >= benchbuildmats[0] && copper >= benchbuildmats[1] && gold >= benchbuildmats[2] && iron >= benchbuildmats[3])
                 {
                     StartCoroutine(BuildBench());
                 }
             }
-            if(vetcaction.DiscreteActions[0] == 5)
+            if (vetcaction.DiscreteActions[0] == 5)
             {
-                if(needtomove(firelocation))
+                if (needtomove(firelocation))
                 {
                 }
-                else if(!campfirebuilt && log >= firebuildmats[0] && iron >= firebuildmats[1])
+                else if (!campfirebuilt && log >= firebuildmats[0] && iron >= firebuildmats[1])
                 {
                     StartCoroutine(BuildFire());
                 }
-                else if(campfirebuilt && log >= cookmats[0] && apple >= cookmats[1] && meat >= cookmats[2] && water >= cookmats[3])
+                else if (campfirebuilt && log >= cookmats[0] && apple >= cookmats[1] && meat >= cookmats[2] && water >= cookmats[3])
                 {
                     StartCoroutine(Cook());
                 }
             }
-            if(vetcaction.DiscreteActions[0] == 6)
+            if (vetcaction.DiscreteActions[0] == 6)
             {
-                if(needtomove(rocketlocation))
+                if (needtomove(rocketlocation))
                 {
                 }
-                else if(!rocketbuilt)
+                else if (!rocketbuilt)
                 //  && log >= rocketbuildmats[0] && copper >= rocketbuildmats[1] && gold >= rocketbuildmats[2] && iron >= rocketbuildmats[3])
                 {
                     StartCoroutine(BuildRocket());
 
                 }
-                else if(rocketbuilt)
+                else if (rocketbuilt)
                 //  && log >= rocketlaunchmats[0] && apple >= rocketlaunchmats[1] && meat >= rocketlaunchmats[2] && oil >= rocketlaunchmats[3] 
                 // && copper >= rocketlaunchmats[4] && gold >= rocketlaunchmats[5] && iron >= rocketlaunchmats[6])
                 {
@@ -429,31 +443,31 @@ public class CharacterMoveScript : Agent
     public override void Heuristic(in ActionBuffers actionsout)
     {
         ActionSegment<int> discreteactions = actionsout.DiscreteActions;
-        if(Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q))
         {
             discreteactions[0] = 0;
         }
-        else if(Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W))
         {
             discreteactions[0] = 1;
         }
-        else if(Input.GetKey(KeyCode.E))
+        else if (Input.GetKey(KeyCode.E))
         {
             discreteactions[0] = 2;
         }
-        else if(Input.GetKey(KeyCode.R))
+        else if (Input.GetKey(KeyCode.R))
         {
             discreteactions[0] = 3;
         }
-        else if(Input.GetKey(KeyCode.T))
+        else if (Input.GetKey(KeyCode.T))
         {
             discreteactions[0] = 4;
         }
-        else if(Input.GetKey(KeyCode.Y))
+        else if (Input.GetKey(KeyCode.Y))
         {
             discreteactions[0] = 5;
         }
-        else if(Input.GetKey(KeyCode.U))
+        else if (Input.GetKey(KeyCode.U))
         {
             discreteactions[0] = 6;
         }
@@ -465,7 +479,7 @@ public class CharacterMoveScript : Agent
 
     public bool needtomove(Transform targetlocation)
     {
-        if(Vector3.Distance(targetlocation.transform.position, controller.transform.position) > 0.9)
+        if (Vector3.Distance(targetlocation.transform.position, controller.transform.position) > 0.9)
         {
             target = targetlocation.position;
             moving = true;
@@ -476,12 +490,40 @@ public class CharacterMoveScript : Agent
         }
     }
 
+    public void toolAction()
+    {
+        if (toolSelector.activeInHierarchy == true)
+        {
+            float toolXposition = toolSelector.GetComponent<RectTransform>().anchoredPosition.x;
+            if (toolXposition >= 340 && toolXposition <= 360)
+            {
+                log_tool = 1;
+            }
+            else
+            {
+                log_tool = 0;
+            }
+        }
+    }
+
+    public void synchAllText()
+    {
+        log = int.Parse(log_t.text.Split('x')[1]);
+        apple = int.Parse(apple_t.text.Split('x')[1]);
+        meat = int.Parse(meat_t.text.Split('x')[1]);
+        oil = int.Parse(oil_t.text.Split('x')[1]);
+        water = int.Parse(water_t.text.Split('x')[1]);
+        copper = int.Parse(copper_t.text.Split('x')[1]);
+        gold = int.Parse(gold_t.text.Split('x')[1]);
+        iron = int.Parse(iron_t.text.Split('x')[1]);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(moving && !busy)
+        if (moving && !busy)
         {
-            if(Vector3.Distance(target, controller.transform.position) > 0.9)
+            if (Vector3.Distance(target, controller.transform.position) > 0.9)
             {
                 input = new Vector2(target.x - controller.transform.position.x, target.z - controller.transform.position.z);
                 input.Normalize();
@@ -499,30 +541,30 @@ public class CharacterMoveScript : Agent
         animator.SetFloat("speed", input.sqrMagnitude);
 
         time = time.AddSeconds(Time.deltaTime * timemulti);
-        UpdateLighting((time.Hour + time.Minute/60f)/24f);
+        UpdateLighting((time.Hour + time.Minute / 60f) / 24f);
         time_t.text = time.ToString("HH:mm");
         day_t.text = (time.Day - 1).ToString();
 
         HealthSlider.value = Health / MaxHealth;
         HungerSlider.value = Hunger / MaxHunger;
         ThirstSlider.value = Thirst / MaxThirst;
-        if(Hunger > 0)
+        if (Hunger > 0)
         {
             Hunger = Hunger - BarSpeedMulti * Time.deltaTime;
         }
-        if(Thirst > 0)
+        if (Thirst > 0)
         {
             Thirst = Thirst - BarSpeedMulti * Time.deltaTime;
         }
-        if(Health > 0 && HungerSlider.value == 0)
+        if (Health > 0 && HungerSlider.value == 0)
         {
             Health = Health - BarSpeedMulti * Time.deltaTime;
         }
-        if(Health > 0 && ThirstSlider.value == 0)
+        if (Health > 0 && ThirstSlider.value == 0)
         {
             Health = Health - BarSpeedMulti * Time.deltaTime;
         }
-        if(Health < 0)
+        if (Health < 0)
         {
             StartCoroutine(Die());
         }
@@ -532,6 +574,8 @@ public class CharacterMoveScript : Agent
         //     rocket.transform.Find("Rocket").Translate(0, -0.2f, 0);
         //     rocket.transform.Find("SmokeEffect").Translate(0, 0.2f, 0);
         // }
+        toolAction();
+        synchAllText();
     }
 
     private void UpdateLighting(float timePercent)
@@ -544,7 +588,7 @@ public class CharacterMoveScript : Agent
 
     private void ApplyGravity()
     {
-        if(controller.isGrounded && velocity < 0)
+        if (controller.isGrounded && velocity < 0)
         {
             velocity = -1;
         }
@@ -567,3 +611,4 @@ public class CharacterMoveScript : Agent
         controller.Move(direction * speed * Time.deltaTime);
     }
 }
+    

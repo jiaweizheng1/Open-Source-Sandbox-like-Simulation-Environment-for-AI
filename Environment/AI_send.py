f31dc9e590d59c1bf8806e2fff68041bc9277b16
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2023-04-29 13:53:04 -0700

    Some error fixes for the previous commit

--- Environment/AI_send.py
+++ Environment/AI_send.py
@@ -1,15 +1,14 @@
 import time
 from utils import *
 
-inputs = [0,0,1,1,2,3,3,3,5,4,6]
+inputs = [0,0,1,1,2,3,3,3]
 
 i = 0
 while True:
     time.sleep(2.5)
-    i+=1
-    if i<=10:
+    if i<=7:
         env_step(inputs[i])
     else:
         i = 0
         env_reset()
-qwwerrrytuiqwwerr
+    i+=1

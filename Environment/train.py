6251b2b0930ad5f736b7a540bfa7a9b4354daf54
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2023-05-03 22:11:48 -0700

    CartPole Working

--- Environment/train.py
+++ Environment/train.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 import torch
 import numpy as np
+import gym
 
 from PPO import PPO
 
@@ -13,18 +14,18 @@ def train():
     print("============================================================================================")
 
     ####### initialize environment hyperparameters ######
-    env_name = "RobotSurvival"
+    env_name = "CartPole-v1"
 
     has_continuous_action_space = False  # continuous action space; else discrete
 
-    max_ep_len = 1000                   # max timesteps in one episode
-    max_training_timesteps = int(3e6)   # break training loop if timeteps > max_training_timesteps
+    max_ep_len = 400                   # max timesteps in one episode
+    max_training_timesteps = int(1e5)   # break training loop if timeteps > max_training_timesteps
 
-    print_freq = max_ep_len * 10        # print avg reward in the interval (in num timesteps)
+    print_freq = max_ep_len * 4        # print avg reward in the interval (in num timesteps)
     log_freq = max_ep_len * 2           # log avg reward in the interval (in num timesteps)
-    save_model_freq = int(1e5)          # save model frequency (in num timesteps)
+    save_model_freq = int(2e5)          # save model frequency (in num timesteps)
 
-    action_std = 0.6                    # starting std for action distribution (Multivariate Normal)
+    action_std = None                    # starting std for action distribution (Multivariate Normal)
     action_std_decay_rate = 0.05        # linearly decay action_std (action_std = action_std - action_std_decay_rate)
     min_action_std = 0.1                # minimum action_std (stop decay after action_std <= min_action_std)
     action_std_decay_freq = int(2.5e5)  # action_std decay frequency (in num timesteps)
@@ -46,6 +47,8 @@ def train():
     #####################################################
 
     print("training environment name : " + env_name)
+
+    env = gym.make(env_name)
 
     # state space dimension
     state_dim = env.observation_space.shape[0]

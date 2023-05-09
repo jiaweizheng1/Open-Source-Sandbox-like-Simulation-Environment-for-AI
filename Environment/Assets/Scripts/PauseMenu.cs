c64ca4fbede59d14da097d1439e437fb1c0e9218
Vincent-WangCH <74845578+Vincent-WangCH@users.noreply.github.com>
2023-05-09 16:24:55 -0700

    Finish the basic in-game menu menu logic

--- Environment/Assets/Scripts/PauseMenu.cs
+++ Environment/Assets/Scripts/PauseMenu.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.MLAgents;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject startMenu;
+    public GameObject optionMenu;
     public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
         pauseMenu.SetActive(false);
+        startMenu.SetActive(true);
+        optionMenu.SetActive(false);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     // Update is called once per frame
@@ -46,11 +53,33 @@ public class PauseMenu : MonoBehaviour
     public void BackToMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        pauseMenu.SetActive(false);
+        /*SceneManager.LoadScene("MainMenu");*/
+        startMenu.SetActive(true);
+        GameObject.Find("Robot").GetComponent<CharacterMoveScript>().EndEpisode();
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void startGame()
+    {
+        startMenu.SetActive(false);
+        Time.timeScale = FindObjectOfType<CharacterMoveScript>().timescale;
+        isPaused = false;
+    }
+
+    public void EnvrionmentEditing()
+    {
+        startMenu.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        startMenu.SetActive(true);
+        optionMenu.SetActive(false);
     }
 }
